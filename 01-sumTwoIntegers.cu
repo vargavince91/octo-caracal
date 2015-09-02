7f422922c5a0eb8528a9872a359e7f30fab26fd2
Vince Varga <vargavince91@gmail.com>
2015-09-02 23:07:37 +0200

    Add program that sums two number on GPU.

---
+++ 01-sumTwoIntegers.cu
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__ void sum(int a, int b, int *c) {
+    *c = a + b;
+}
+
+int main (void) {
+
+    int a, b, c;
+
+    int *device_c;
+    cudaMalloc((void**) &device_c, sizeof(int));
+
+    printf("This program calculates the sum of two numbers using GPU.\n");
+
+    printf("a=");
+    scanf("%d", &a);
+    printf("b=");
+    scanf("%d", &b);
+
+    sum<<<1,1>>>(a,b,device_c);
+
+    cudaMemcpy(&c, device_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("%d+%d = %d\n", a, b, c);
+
+    cudaFree(device_c);
+
+    return 0;
+}
